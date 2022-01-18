879d6951e487c725360c337e444947978366d6e1
Kleverson Eller <kleverson.a16@gmail.com>
2022-01-17 22:36:08 -0300

    Requisito - 7

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,8 +33,22 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 1 ], [ 'sub', 0 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => ({
+    sum: number1 + number2,
+    mult: number1 * number2,
+    div: Math.floor(number1 / number2),
+    sub: number1 - number2,
+  });
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {
+  switch (type) {
+    case 'keys':
+      return Object.keys(object);
+    case 'values':
+      return Object.values(object);
+    default:
+      return Object.entries(object);
+  }
+};
 
 module.exports = { calculator, arrayGenerator };
